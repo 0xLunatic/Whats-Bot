e75a817263831520d02afb61980ad8b69528d96b
DreamerDotID <49750854+DreamerDotID@users.noreply.github.com>
2022-11-04 00:09:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,6 @@ client.on("message", (msg) => {
         return response.json();
       })
       .then(function (data) 
-        console.log("Work");
         msg.reply('"' + data.result + '"');
       });
   }
